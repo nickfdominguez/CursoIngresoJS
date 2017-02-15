d1b04e2a64917fe93598387dcebc425e8d5c8b89
nickfdominguez <nickfdominguez@gmail.com>
2017-02-15 20:01:10 -0300

    Add files via upload

--- 8-TPs/jsFerreteConstruccion.js
+++ 8-TPs/jsFerreteConstruccion.js
@@ -7,12 +7,47 @@ C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de c
 function Rectangulo () 
 {
 
+		var base;
+		var altura;
+		var perimetro;
+
+		base = document.getElementById('Largo').value;
+		altura = document.getElementById('Ancho').value;
+		base = parseInt(base);
+		altura = parseInt(altura);
+
+		perimetro = (2 * altura * base) * 3;
+
+		alert(perimetro);
 }
 function Circulo () 
 {
+		var radio;
+		var circulo;
+
+		radio = document.getElementById('Radio').value;
+		radio = parseInt(radio);
+
+		circulo = (2 * 3.14 * radio) * 3;
 	
+		alert(circulo);
 }
 function Materiales () 
 {
-	
+		var base;
+		var altura;
+
+		var bolsaDeCemento;
+		var bolsaDeCal;
+
+		base = document.getElementById('Largo').value;
+		altura = document.getElementById('Ancho').value;
+		base = parseInt(base);
+		altura = parseInt(altura);
+
+		bolsaDeCemento = 2 * base * altura;
+		bolsaDeCal = 3 * base * altura;
+
+		alert("Se necesitan " + bolsaDeCemento + " bolsas de cemento y " + bolsaDeCal + " bolsas de cal");
+
 }
--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -5,13 +5,54 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
-	
+		var precio1;
+		var precio2;
+		var precio3;
+		var resultado;
+
+		precio1 = document.getElementById('PrecioUno').value;
+		precio2 = document.getElementById('PrecioDos').value;
+		precio3 = document.getElementById('PrecioTres').value;
+
+		resultado = parseInt(precio1) + parseInt(precio2) + parseInt(precio3);
+
+		alert(resultado);
 }
 function Promedio () 
 {
-	
+		var precio1;
+		var precio2;
+		var precio3;
+		var resultado;
+
+		precio1 = document.getElementById('PrecioUno').value;
+		precio2 = document.getElementById('PrecioDos').value;
+		precio3 = document.getElementById('PrecioTres').value;
+
+		precio1 = parseInt(precio1);
+		precio2 = parseInt(precio2);
+		precio3 = parseInt(precio3);
+
+		resultado = (precio1 + precio2 + precio3)/3;
+
+		alert(resultado);
 }
 function PrecioFinal () 
 {
-	
+		var precio1;
+		var precio2;
+		var precio3;
+		var resultado;
+
+		precio1 = document.getElementById('PrecioUno').value;
+		precio2 = document.getElementById('PrecioDos').value;
+		precio3 = document.getElementById('PrecioTres').value;
+
+		precio1 = parseInt(precio1);
+		precio2 = parseInt(precio2);
+		precio3 = parseInt(precio3);
+
+		resultado = (precio1 + precio2 + precio3)/1.21;	
+
+		alert(resultado);
 }
