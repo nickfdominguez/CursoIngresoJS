0e1c7142e4bd7a9477c304e6706a14ed01b687fb
nickfdominguez <nickfdominguez@gmail.com>
2019-02-15 09:15:15 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -1,7 +1,18 @@
 function mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+    var mesDelaño;
+    mesDelaño=document.getElementById("mes").value;
+    alert(mesDelaño);
+    switch(mesDelaño)
+    {
+        case "Febrero":
+        alert("tiene 28 dias");
+        break;
+
+        case "Abril":
+        case "Junio":
+    }
 
 //alert (mesDelAño);
 	
